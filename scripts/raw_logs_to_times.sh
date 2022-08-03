92837d030b39e060db545fa66b8584a07bfc7e9a
Martin Blicha <martin.blicha@usi.ch>
2022-08-03 16:53:24 +0200

    Fix script for creating results

--- scripts/raw_logs_to_times.sh
+++ scripts/raw_logs_to_times.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 extract_script="./extract_times.sh"
-log_dir="raw_logs"
-times_dir="times"
+log_dir="${ARTIFACT_ROOT}/raw_logs"
+times_dir="${ARTIFACT_ROOT}/times"
 
 for top_dir in ${log_dir}/multi-phase_safe ${log_dir}/multi-phase_unsafe ${log_dir}/LRA-TS; do
     if [ ! -d ${top_dir} ]; then
@@ -10,6 +10,7 @@ for top_dir in ${log_dir}/multi-phase_safe ${log_dir}/multi-phase_unsafe ${log_d
     fi
     bench_dir_name=`basename ${top_dir}`
     echo "Processing benchmark directory ${bench_dir_name}"
+    mkdir -p ${times_dir}/${bench_dir_name}
     for tool_dir in ${top_dir}/*; do
         tool_name=`basename ${tool_dir}`
         echo "Processing tool ${tool_name}"
